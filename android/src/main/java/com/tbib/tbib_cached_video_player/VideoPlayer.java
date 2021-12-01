d776376e22d0d68f30b5246de51bb4a13a916ec8
BIGman <52568890+tudosxxx@users.noreply.github.com>
2021-12-01 13:14:20 +0800

    Update VideoPlayer.java
    300 minFileSize

--- android/src/main/java/com/tbib/tbib_cached_video_player/VideoPlayer.java
+++ android/src/main/java/com/tbib/tbib_cached_video_player/VideoPlayer.java
@@ -74,7 +74,7 @@ final class VideoPlayer {
               context,
                   // TODO: need a way to set these programmatically. Maybe fork VideoPlayerPlatformInterface
                   1024*1024*1024,
-                  1024*1024*100);
+                  1024*1024*300);
       if (httpHeaders != null && !httpHeaders.isEmpty()) {
         cacheDataSourceFactory.setHeaders(httpHeaders);
       }
